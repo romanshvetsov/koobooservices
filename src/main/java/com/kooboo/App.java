a9ba04044e42d48e0e81ca21b6f0da196e469ef4
romanshvetsov <romanshvetsov@romanshvetsovs-MacBook-Pro.local>
2022-03-27 21:33:30 +0300

    Initial commit

---
+++ src/main/java/com/kooboo/App.java
@@ -0,0 +1,13 @@
+package com.kooboo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
